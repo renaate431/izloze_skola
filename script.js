8694c1f86ac5ff0cfdb1417e52e84a28db9878ac
renaate431 <r.goldmane@gmail.com>
2022-03-08 10:18:34 +0200

    pirmais

---
+++ script.js
@@ -0,0 +1,7 @@
+constvardi=['Jana Kalnina', 'toms brivmurnieks', 'roberts lengdons', 'luize freska', 'andris dridzis'];
+const balvas=['phone', 'house', 'ledusskapis', 'kamera', 'TV'];
+const naudaKopa=100000000000;
+let uzvaretajuSkaits=5;
+Math.random=Math.raandom()*10;
+rand=Math.floor(rand); // noapaļo uz leju
+console.log(vardi[rand]);
